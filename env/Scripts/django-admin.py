a79443f15b74827aaa98f8de837e0282523a2f47
mjrq <mariano.roque.masi@es.ey.com>
2020-05-06 20:51:21 +0200

    asd

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\xc681rh\Desktop\PoC FinCrime\ProyectoVisualStudio\ProyectoVS_PoCFinCrime\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

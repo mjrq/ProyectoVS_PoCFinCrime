b7d4439b0efe13a1fde60f39b044561f3edb5a82
mjrq <mariano.roque.masi@es.ey.com>
2020-05-07 08:19:55 +0200

    asd

--- ProyectoVS_PoCFinCrime/settings.py
+++ ProyectoVS_PoCFinCrime/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '550f194e-b038-491c-8ce9-47f90f5b52c1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application references
 # https://docs.djangoproject.com/en/2.1/ref/settings/#std:setting-INSTALLED_APPS

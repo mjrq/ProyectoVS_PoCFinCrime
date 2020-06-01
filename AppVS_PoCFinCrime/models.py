06eccc87183441a0199e6a76d6299cfb63522817
Inmaculada Valero <Inmaculada.Valero.Frias@met.ey.com>
2020-06-01 15:31:34 +0200

    1ªTable

--- AppVS_PoCFinCrime/models.py
+++ AppVS_PoCFinCrime/models.py
@@ -4,110 +4,106 @@ from django.views.generic import ListView
 # Create your models here.
 
 class CUSTOMERS(models.Model):
-    CUSTOMER_SOURCE_UNIQUE_ID = models.CharField(max_length=100,primary_key=True,)
-    ACCOUNT_BALANCE = models.FloatField(blank=True, null=True)
-    ACCOUNT_ON_HOLD_FLAG = models.CharField(max_length=100)#
-    ACCOUNT_PURPOSE = models.TextField()
-    ACCOUNT_SEGMENT = models.IntegerField()
+    ACCOUNT_BALANCE = models.FloatField(blank=True,null=True)
+    ACCOUNT_ON_HOLD_FLAG = models.CharField(max_length=100,blank=True, null=True)#
+    ACCOUNT_PURPOSE = models.TextField(blank=True, null=True)
+    ACCOUNT_SEGMENT = models.IntegerField(blank=True, null=True)
     ACCOUNT_SOURCE_UNIQUE_ID = models.TextField(unique = True)
-    ACCOUNT_STATUS_CODE = models.CharField(max_length=100)
-    ACQUISITION_DATE = models.DateField()
-    ANN_PREMIUM_AMT = models.CharField(max_length=100)#
-    BALANCE_DATE = models.DateField(auto_now=False, auto_now_add =False)
-    BENEFICIARY_FLAG = models.CharField(max_length=100)
-    BLOCKED_REASON = models.TextField()
-    BLOCKED_TYPE = models.CharField(max_length=100)
-    BRANCH_ID = models.IntegerField()
-    BUSINESS_SEGMENT_1 = models.CharField(max_length=100)#
-    BUSINESS_TYPE = models.TextField()
-    CANCELLED_DATE = models.DateField(auto_now=False, auto_now_add =False)
-    CASH_SURR_VALUE = models.CharField(max_length=100)#
-    CHANNEL_OF_OPENING = models.CharField(max_length=100)
-    CHECK_ACCOUNT_NUMBER = models.IntegerField()
-    CITY = models.TextField()
-    COLLECTED_BALANCE = models.FloatField()
-    COMMENTS = models.TextField()
-    COMPANY_FORM  = models.CharField(max_length=100)
-    COMPANY_NAME = models.TextField()#
-    COUNTRY_OF_ORIGIN = models.CharField(max_length=100)
-    COUNTRY_OF_RESIDENCE = models.CharField(max_length=100)
-    CREDIT_DEBIT_CODE = models.CharField(max_length=100)
-    CREDIT_LIMIT = models.FloatField()
-    CREDIT_TURNOVER = models.CharField(max_length=100)
-    CURRENCY_CODE = models.CharField(max_length=100)
-    CURRENCY_CODE_BASE = models.CharField(max_length=100)
-    CURRENCY_CODE_BASE = models.CharField(max_length=100)
-    CURRENCY_CODE_ORIG = models.CharField(max_length=100)
-    CUSTOMER_INITIATED_CLOSURE = models.CharField(max_length=100)#
-    CUSTOMER_SEGMENT_1 = models.IntegerField()
-    CUSTOMER_SOURCE_UNIQUE_ID = models.TextField(unique = True)
-    CUSTOMER_SOURCE_UNIQUE_ID = models.TextField(unique = True)
-    CUSTOMER_STATUS_CODE = models.IntegerField()
-    CUSTOMER_TYPE = models.CharField(max_length=100)
-    CUSTOMER_TYPE_CODE = models.CharField(max_length=100)
-    DATE_CLOSED = models.DateField(auto_now=False, auto_now_add =False)
-    DATE_OPENED = models.DateField(auto_now=False, auto_now_add =False)
-    DEATH_BENEFIT_AMT = models.CharField(max_length=100)#
-    DEBIT_TRUNOVER = models.CharField(max_length=100)
-    DELAYED_ACCOUNT_FLAG = models.CharField(max_length=100)#
-    DELIVERY_INSTRUCTIONS = models.CharField(max_length=100)#
-    DEVICE_ID = models.TextField(unique = True)#
-    EMPLOYEE_FLAG = models.CharField(max_length=100)
-    EMPLOYEE_NUMBER = models.IntegerField()
-    EMPLOYMENT_STATUS = models.CharField(max_length=100)
-    ERROR_CORRECT_FLAG = models.CharField(max_length=100)#
-    FILTER = models.CharField(max_length=100)#
-    FROM_DATE = models.DateField(auto_now=False, auto_now_add =False)
-    GENDER_CODE = models.CharField(max_length=100)
-    GROSS_PREM_TTD = models.CharField(max_length=100)#
-    HOLDING_BANK_NAME = models.TextField()
-    INCORPORATION_COUNTRY_CODE = models.CharField(max_length=100)
-    INCORPORATION_DATE = models.DateField(auto_now=False, auto_now_add =False)
-    INST_PREMIUM_AMT = models.CharField(max_length=100)
-    ISSUE_DATE = models.DateField(auto_now=False, auto_now_add =False)
-    LANGUAGE_PREF_CODE = models.CharField(max_length=100)#
-    LIFE_INS_CONTRACT_DURATION = models.IntegerField()
-    MATURITY_DATE = models.DateField(auto_now=False, auto_now_add =False)
-    OCCUPATION = models.TextField()
-    ORG_UNIT_CODE = models.CharField(max_length=100)
-    ORG_UNIT_CODE = models.CharField(max_length=100)
-    ORIGINATION_DATE = models.DateField(auto_now=False, auto_now_add =False)
-    OTHER_HOLD_AMOUNT = models.FloatField()
-    OVERDRAFT_LIMIT = models.FloatField()
-    PAYMENT_FREQ = models.CharField(max_length=100)
-    PAYMENT_METHOD = models.TextField()
-    PERIODIC_LOAN_AMOUNT = models.FloatField()
-    PERSON_TITLE = models.TextField()
-    POSTAL_CODE = models.IntegerField()
-    PRIMARY_CARD_ID = models.IntegerField()
-    PRIMARY_CUSTOMER_CATEGORY_CODE = models.IntegerField()
-    PRIME_BRANCH_ID = models.IntegerField()
-    PRODUCT_SOURCE_TYPE_CODE = models.IntegerField()
-    PRODUCT_SOURCE_TYPE_CODE = models.IntegerField()
-    REGISTERED_NUMBER = models.IntegerField()#
-    RELATIONSHIP_MGR_ID = models.IntegerField()
-    REPORTING_REGION = models.TextField()
-    RISK_CATEGORY = models.CharField(max_length=100)
-    RISK_SCORE = models.IntegerField()
-    RUN_TIMESTAMP = models.CharField(max_length=100)#
-    SALARY_LODGED_FLAG = models.CharField(max_length=100)#
-    SENT_CORRESPONDENCE_FLAG = models.CharField(max_length=100)#
-    SETTLEMENT_BANK_NAME = models.CharField(max_length=100)#
-    SETTLEMENT_TYPE = models.CharField(max_length=100)#
-    SINGLE_PREMIUM_TOTAL = models.CharField(max_length=100)#
-    SOURCE_SYSTEM_CODE = models.CharField(max_length=100)#
-    SOURCE_TXN_NUM = models.IntegerField()
-    SOURCE_TXN_UNIQUE_ID = models.TextField(unique = True)
-    SUB_DIVISION = models.IntegerField()
-    SUM_INSURED = models.CharField(max_length=100)#
-    TO_DATE = models.DateField(auto_now=False, auto_now_add =False)
-    TOTAL_DEPOSITS_BASE = models.IntegerField()
-    TOTAL_LOANS_BASE = models.IntegerField()
-    TRANSACTION_LOCATION = models.TextField()#
-    TURNOVER_FROM_DATE = models.DateField(auto_now=False, auto_now_add =False)
-    TURNOVER_TO_DATE = models.DateField(auto_now=False, auto_now_add =False)
-    TXN_AMOUNT_ORIG = models.FloatField()
-    TXN_CHANNEL_CODE = models.IntegerField()
-    TXN_SOURCE_TYPE_CODE = models.CharField(max_length=100)
-    TXN_STATUS_CODE = models.CharField(max_length=100)
-    ZONE = models.TextField()
+    ACCOUNT_STATUS_CODE = models.CharField(max_length=100, blank=True, null=True)
+    ACQUISITION_DATE = models.DateField(blank=True, null=True)
+    ANN_PREMIUM_AMT = models.CharField(max_length=100, blank=True, null=True)#
+    BALANCE_DATE = models.DateField(blank=True, null=True)
+    BENEFICIARY_FLAG = models.CharField(max_length=100, blank=True, null=True)
+    BLOCKED_REASON = models.TextField(blank=True, null=True)
+    BLOCKED_TYPE = models.CharField(max_length=100, blank=True, null=True)
+    BRANCH_ID = models.IntegerField(blank=True, null=True)
+    BUSINESS_SEGMENT_1 = models.TextField(blank=True, null=True)#
+    BUSINESS_TYPE = models.TextField(blank=True, null=True)
+    CANCELLED_DATE = models.DateField(null=True)
+    CASH_SURR_VALUE = models.CharField(max_length=100, blank=True, null=True)#
+    CHANNEL_OF_OPENING = models.CharField(max_length=100, blank=True, null=True)
+    CHECK_ACCOUNT_NUMBER = models.IntegerField( blank=True, null=True)
+    CITY = models.TextField( blank=True, null=True)
+    COLLECTED_BALANCE = models.FloatField(null=True)
+    COMMENTS = models.TextField( blank=True, null=True)
+    COMPANY_FORM  = models.CharField(max_length=100,  blank=True, null=True)
+    COMPANY_NAME = models.TextField( blank=True, null=True)#
+    COUNTRY_OF_ORIGIN = models.CharField(max_length=100,  blank=True, null=True)
+    COUNTRY_OF_RESIDENCE = models.CharField(max_length=100,  blank=True, null=True)
+    CREDIT_DEBIT_CODE = models.CharField(max_length=100,  blank=True, null=True)
+    CREDIT_LIMIT = models.FloatField( blank=True, null=True)
+    CREDIT_TURNOVER = models.CharField(max_length=100,  blank=True, null=True)
+    CURRENCY_CODE = models.CharField(max_length=100, blank=True, null=True)
+    CURRENCY_CODE_BASE = models.CharField(max_length=100, blank=True, null=True)
+    CURRENCY_CODE_ORIG = models.CharField(max_length=100, blank=True, null=True)
+    CUSTOMER_INITIATED_CLOSURE = models.CharField(max_length=100, blank=True, null=True)#
+    CUSTOMER_SEGMENT_1 = models.IntegerField(blank=True, null=True)
+    CUSTOMER_SOURCE_UNIQUE_ID = models.TextField(blank=True, null=True)
+    CUSTOMER_ID = models.TextField(blank=True, null=True)
+    CUSTOMER_STATUS_CODE = models.IntegerField( blank=True, null=True)
+    CUSTOMER_TYPE = models.CharField(max_length=100,  blank=True, null=True)
+    CUSTOMER_TYPE_CODE = models.CharField(max_length=100,  blank=True, null=True)
+    DATE_CLOSED = models.DateField(blank=True, null=True)
+    DATE_OPENED = models.DateField(blank=True, null=True)
+    DEATH_BENEFIT_AMT = models.CharField(max_length=100,  blank=True, null=True)#
+    DEBIT_TRUNOVER = models.CharField(max_length=100,  blank=True, null=True)
+    DELAYED_ACCOUNT_FLAG = models.CharField(max_length=100,  blank=True, null=True)#
+    DELIVERY_INSTRUCTIONS = models.TextField(blank=True, null=True)#
+    DEVICE_ID = models.TextField(blank=True, null=True)#
+    EMPLOYEE_FLAG = models.CharField(max_length=100,  blank=True, null=True)
+    EMPLOYEE_NUMBER = models.IntegerField( blank=True, null=True)
+    EMPLOYMENT_STATUS = models.CharField(max_length=100,  blank=True, null=True)
+    ERROR_CORRECT_FLAG = models.CharField(max_length=100,  blank=True, null=True)#
+    FILTER = models.CharField(max_length=100,  blank=True, null=True)#
+    FROM_DATE = models.DateField(blank=True, null=True)
+    GENDER_CODE = models.CharField(max_length=100,  blank=True, null=True)
+    GROSS_PREM_TTD = models.CharField(max_length=100,  blank=True, null=True)#
+    HOLDING_BANK_NAME = models.TextField( blank=True, null=True)
+    INCORPORATION_COUNTRY_CODE = models.CharField(max_length=100,  blank=True, null=True)
+    INCORPORATION_DATE = models.DateField(blank=True, null=True)
+    INST_PREMIUM_AMT = models.CharField(max_length=100,  blank=True, null=True)
+    ISSUE_DATE = models.DateField(blank=True, null=True)
+    LANGUAGE_PREF_CODE = models.CharField(max_length=100,  blank=True, null=True)#
+    LIFE_INS_CONTRACT_DURATION = models.IntegerField( blank=True, null=True)
+    MATURITY_DATE = models.DateField(blank=True, null=True)
+    OCCUPATION = models.TextField( blank=True, null=True)
+    ORG_UNIT_CODE = models.CharField(max_length=100,  blank=True, null=True)
+    ORIGINATION_DATE = models.DateField(auto_now=False, auto_now_add =False,  blank=True, null=True)
+    OTHER_HOLD_AMOUNT = models.FloatField( blank=True, null=True)
+    OVERDRAFT_LIMIT = models.FloatField( blank=True, null=True)
+    PAYMENT_FREQ = models.CharField(max_length=100,  blank=True, null=True)
+    PAYMENT_METHOD = models.TextField( blank=True, null=True)
+    PERIODIC_LOAN_AMOUNT = models.FloatField( blank=True, null=True)
+    PERSON_TITLE = models.TextField( blank=True, null=True)
+    POSTAL_CODE = models.IntegerField( blank=True, null=True)
+    PRIMARY_CARD_ID = models.IntegerField( blank=True, null=True)
+    PRIMARY_CUSTOMER_CATEGORY_CODE = models.TextField( blank=True, null=True)
+    PRIME_BRANCH_ID = models.IntegerField( blank=True, null=True)
+    PRODUCT_SOURCE_TYPE_CODE = models.CharField(max_length=100, blank=True, null=True)
+    REGISTERED_NUMBER = models.IntegerField( blank=True, null=True)#
+    RELATIONSHIP_MGR_ID = models.IntegerField( blank=True, null=True)
+    REPORTING_REGION = models.TextField( blank=True, null=True)
+    RISK_CATEGORY = models.CharField(max_length=100,  blank=True, null=True)
+    RISK_SCORE = models.IntegerField(blank=True, null=True)
+    RUN_TIMESTAMP = models.CharField(max_length=100, blank=True, null=True)#
+    SALARY_LODGED_FLAG = models.CharField(max_length=100, blank=True, null=True)#
+    SENT_CORRESPONDENCE_FLAG = models.CharField(max_length=100, blank=True, null=True)#
+    SETTLEMENT_BANK_NAME = models.TextField(blank=True, null=True)#
+    SETTLEMENT_TYPE = models.CharField(max_length=100, blank=True, null=True)#
+    SINGLE_PREMIUM_TOTAL = models.CharField(max_length=100,  blank=True, null=True)#
+    SOURCE_SYSTEM_CODE = models.CharField(max_length=100,  blank=True, null=True)#
+    SOURCE_TXN_NUM = models.TextField( blank=True, null=True)
+    SOURCE_TXN_UNIQUE_ID = models.TextField(unique = True, primary_key=True)
+    SUB_DIVISION = models.IntegerField( blank=True, null=True)
+    SUM_INSURED = models.CharField(max_length=100,  blank=True, null=True)#
+    TO_DATE = models.DateField(blank=True, null=True)
+    TOTAL_DEPOSITS_BASE = models.IntegerField( blank=True, null=True)
+    TOTAL_LOANS_BASE = models.IntegerField( blank=True, null=True)
+    TRANSACTION_LOCATION = models.TextField( blank=True, null=True)#
+    TURNOVER_FROM_DATE = models.DateField(blank=True, null=True)
+    TURNOVER_TO_DATE = models.DateField(blank=True, null=True)
+    TXN_AMOUNT_ORIG = models.FloatField( blank=True, null=True)
+    TXN_CHANNEL_CODE = models.IntegerField( blank=True, null=True)
+    TXN_SOURCE_TYPE_CODE = models.CharField(max_length=100,  blank=True, null=True)
+    TXN_STATUS_CODE = models.CharField(max_length=100,  blank=True, null=True)
+    ZONE = models.TextField( blank=True, null=True)
--- AppVS_PoCFinCrime/views.py
+++ AppVS_PoCFinCrime/views.py
@@ -10,6 +10,7 @@ from django.views import generic
 
 import csv, io
 import codecs
+from datetime import datetime
 from django.shortcuts import render
 from django.contrib import messages
 
@@ -19,7 +20,7 @@ def CargarClientes(request):
     # declaring template
     template = "AppVS_PoCFinCrime/CargarClientes.html"
     data = CUSTOMERS.objects.all()
-# prompt is a context variable that can have different values depending on their context
+    #prompt is a context variable that can have different values depending on their context
     prompt = {
         'order': 'Order of the CSV should be name, email, address,    phone, profile',
         'profiles': data,
@@ -28,29 +29,138 @@ def CargarClientes(request):
     # GET request returns the value of the data with the specified key.
     if request.method == "GET":
         return render(request, template, prompt)
+
     csv_file = request.FILES['file']
     # let's check if it is a csv file
     if not csv_file.name.endswith('.csv'):
         messages.error(request, 'THIS IS NOT A CSV FILE')
-   
+
+    
+    # Read complete CSV
     data_set = csv_file.read().decode('latin-1')
-   
 
-    
+    # Read CSV lines 
     lines = data_set.split("\n")
-    #lines = lines.split(";")
 
-    print(lines)
-    
+    #Get the data from CSV
     for line in lines:
-        fields=line.split(";")
-       #fields=line.replace("\r"," ")
-       #print(fields)
-        CUSTOMER = CUSTOMERS( CUSTOMER_SOURCE_UNIQUE_ID = fields[1] )
+        #Data separate by "|"
+        data=line.split("|")
+        
+        #Convert empty fields in nullable data and Create a list with all data.
+        fields =[]
+        for i in data:
+            if i=='':
+                fields.append(None)
+            else:
+                fields.append(i)
+        #Create table
+        CUSTOMER = CUSTOMERS(ACCOUNT_BALANCE =float(fields[0]) if (fields[0] is not None) else fields[0],
+                             ACCOUNT_ON_HOLD_FLAG = fields[1],
+                             ACCOUNT_PURPOSE =fields[2],
+                             ACCOUNT_SEGMENT =int(fields[3]),
+                             ACCOUNT_SOURCE_UNIQUE_ID =fields[4],
+                             ACCOUNT_STATUS_CODE =fields[5],
+                             ACQUISITION_DATE = datetime.strptime(fields[6],'%Y'),
+                             ANN_PREMIUM_AMT = fields[7],
+                             BALANCE_DATE = datetime.strptime(fields[8],'%Y'),
+                             BENEFICIARY_FLAG =fields[9],
+                             BLOCKED_REASON =  fields[10],                                 
+                             BLOCKED_TYPE = fields[11],                                  
+                             BRANCH_ID =int(fields[12]),
+                             BUSINESS_SEGMENT_1 = fields[13],
+                             BUSINESS_TYPE =fields[14],
+                             CANCELLED_DATE = fields[15],
+                             CASH_SURR_VALUE = fields[16],
+                             CHANNEL_OF_OPENING = fields[17],
+                             CHECK_ACCOUNT_NUMBER = int(fields[18]),
+                             CITY = fields[19],
+                             COLLECTED_BALANCE =float(fields[20])  if (fields[20] is not None) else fields[20], 
+                             COMMENTS =fields[21], 
+                             COMPANY_FORM  = fields[22], 
+                             COMPANY_NAME =fields[23], 
+                             COUNTRY_OF_ORIGIN = fields[24], 
+                             COUNTRY_OF_RESIDENCE = fields[25], 
+                             CREDIT_DEBIT_CODE =fields[26],
+                             CREDIT_LIMIT = float(fields[27]) if (fields[27] is not None) else fields[27],
+                             CREDIT_TURNOVER = fields[28],
+                             CURRENCY_CODE =fields[29], 
+                             CURRENCY_CODE_BASE = fields[30],
+                             CURRENCY_CODE_ORIG = fields[31],
+                             CUSTOMER_INITIATED_CLOSURE = fields[32],
+                             CUSTOMER_SEGMENT_1 =int(fields[33]),
+                             CUSTOMER_SOURCE_UNIQUE_ID = fields[34],
+                             CUSTOMER_ID =fields[35],
+                             CUSTOMER_STATUS_CODE = int(fields[36]),
+                             CUSTOMER_TYPE = fields[37],
+                             CUSTOMER_TYPE_CODE = fields[38],
+                             DATE_CLOSED = datetime.strptime(fields[39],'%d/%m/%Y'),
+                             DATE_OPENED = datetime.strptime(fields[40],'%d/%m/%Y'),
+                             DEATH_BENEFIT_AMT =fields[41],
+                             DEBIT_TRUNOVER =fields[42],
+                             DELAYED_ACCOUNT_FLAG = fields[43],
+                             DELIVERY_INSTRUCTIONS = fields[44],
+                             DEVICE_ID = fields[45],
+                             EMPLOYEE_FLAG = fields[46],
+                             EMPLOYEE_NUMBER = fields[47],
+                             EMPLOYMENT_STATUS = fields[48],
+                             ERROR_CORRECT_FLAG =fields[49],
+                             FILTER = fields[50],
+                             FROM_DATE =datetime.strptime(fields[51],'%d/%m/%Y'),
+                             GENDER_CODE = fields[52],
+                             GROSS_PREM_TTD =fields[53],
+                             HOLDING_BANK_NAME = fields[54],
+                             INCORPORATION_COUNTRY_CODE = fields[55],
+                             INCORPORATION_DATE = datetime.strptime(fields[56],'%d/%m/%Y'),
+                             INST_PREMIUM_AMT = fields[57],
+                             ISSUE_DATE = datetime.strptime(fields[58],'%d/%m/%Y'),
+                             LANGUAGE_PREF_CODE = fields[59],
+                             LIFE_INS_CONTRACT_DURATION = int(fields[60]),
+                             MATURITY_DATE =fields[61],
+                             OCCUPATION =fields[62],
+                             ORG_UNIT_CODE = fields[63],
+                             ORIGINATION_DATE =datetime.strptime(fields[64],'%d/%m/%Y'),
+                             OTHER_HOLD_AMOUNT = float(fields[65])  if (fields[65] is not None) else fields[65],
+                             OVERDRAFT_LIMIT =float(fields[66])  if (fields[66] is not None) else fields[66],
+                             PAYMENT_FREQ = fields[67],
+                             PAYMENT_METHOD = fields[68],
+                             PERIODIC_LOAN_AMOUNT =float(fields[69]  if (fields[69] is not None) else fields[69]),
+                             PERSON_TITLE = fields[70],
+                             POSTAL_CODE =int(fields[71]),
+                             PRIMARY_CARD_ID =fields[72],
+                             PRIMARY_CUSTOMER_CATEGORY_CODE = fields[73],
+                             PRIME_BRANCH_ID =int(fields[74]),
+                             PRODUCT_SOURCE_TYPE_CODE = fields[75],
+                             REGISTERED_NUMBER =fields[76],
+                             RELATIONSHIP_MGR_ID =int(fields[77]),
+                             REPORTING_REGION =fields[78],
+                             RISK_CATEGORY = fields[79],
+                             RISK_SCORE =int(fields[80]),
+                             RUN_TIMESTAMP =fields[81],
+                             SALARY_LODGED_FLAG = fields[82],
+                             SENT_CORRESPONDENCE_FLAG =fields[83],
+                             SETTLEMENT_BANK_NAME =fields[84],
+                             SETTLEMENT_TYPE = fields[85],
+                             SINGLE_PREMIUM_TOTAL = fields[86],
+                             SOURCE_SYSTEM_CODE = fields[87],
+                             SOURCE_TXN_NUM = fields[88],
+                             SOURCE_TXN_UNIQUE_ID = fields[89],
+                             SUB_DIVISION =fields[90],
+                             SUM_INSURED =fields[91],
+                             TO_DATE = fields[92],
+                             TOTAL_DEPOSITS_BASE = fields[93],
+                             TOTAL_LOANS_BASE = int(fields[94]),
+                             TRANSACTION_LOCATION = fields[95],
+                             TURNOVER_FROM_DATE =fields[96],
+                             TURNOVER_TO_DATE =fields[97],
+                             TXN_AMOUNT_ORIG = float(fields[98])  if (fields[98] is not None) else fields[98],
+                             TXN_CHANNEL_CODE = fields[99],
+                             TXN_SOURCE_TYPE_CODE = fields[100],
+                             TXN_STATUS_CODE = fields[101],
+                             ZONE = fields[102]                                
+                             )
         CUSTOMER.save()
 
-
-    #ACCOUNT_BALANCE=fields[2],ACCOUNT_ON_HOLD_FLAG =fields[3],ACCOUNT_PURPOSE=fields[3], ACCOUNT_SEGMENT=fields[3] , ACCOUNT_SOURCE_UNIQUE_ID =fields[3]
     context = {}
     template2 = "AppVS_PoCFinCrime/FicheroCargado.html" 
     return render(request, template2, context)
@@ -68,7 +178,7 @@ def index(request):
         "AppVS_PoCFinCrime/index.html",  # Relative path from the 'templates' folder to the template file
         # "index.html", # Use this code for VS 2017 15.7 and earlier
         {
-             'title' : "Inicio",
+            'title' : "Inicio",
             'message' : "Pagina Principal PoCFinCrime",
            
         }
